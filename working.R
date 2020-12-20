f9479decb3497b4aa012dacd20542ce376439199
mengfanli1127 <limengfan1127@gmail.com>
2020-12-20 22:37:23 +0800

    manche Rechtschreibfehler

--- working.R
+++ working.R
@@ -32,16 +32,17 @@ View(cases.per.day.relative100000)
 cases.per.day.relative1 <- daily.cases(Daten, end = "2020-11-14", relative = TRUE, reverenz = 1)
 View(cases.per.day.relative1)
 
+
 ##Bilden von Inzidenz Tabelle (Beispiele)
 incidence30 <- daily.incedence(Daten, end = "2020-11-14", intervall = 30, relative = FALSE)
-View(incedence30)
+View(incidence30)
 
 incidence7 <- daily.incedence(Daten, end = "2020-11-14", intervall = 7, relative = FALSE)
-View(incedence7)
+View(incidence7)
 
 incidence3 <- daily.incedence(Daten, end = "2020-11-14", intervall = 3, relative = FALSE)
-View(incedence3)
+View(incidence3)
 
 incidence7abs <- daily.incedence(Daten, end = "2020-11-14", intervall = 7, relative = TRUE)
-View(incedence7abs)
+View(incidence7abs)
 
