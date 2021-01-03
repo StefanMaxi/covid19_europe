88248853f8f7caeb038f66514e545017f8fb9817
sunmumu <58950430+szilvias@users.noreply.github.com>
2021-01-03 16:38:19 +0100

    balkendiagramm

--- working.R
+++ working.R
@@ -452,4 +452,50 @@ mat_sex <- matrix(
 )
 knitr::kable(mat_sex)
 
+#Balkendiagramm Fälle pro Tag Vorpräsentation 
+#Anpassen der Daten 
+cases.per.day.belgium <- select(cases.per.day, -Bayern, -Schweden, -Tschechien)
+cases.per.day.bayern <- select(cases.per.day, -Belgien, -Schweden, -Tschechien) 
+cases.per.day.schweden <- select(cases.per.day, -Bayern, -Belgien, -Tschechien) 
+cases.per.day.tschechien <- select(cases.per.day, -Bayern, -Schweden, -Belgien) 
+
+#Belgien
+balken.belgien <- ggplot(cases.per.day.belgium, aes(Datum, Belgien))+
+  geom_col(fill = "steelblue")+
+  ggtitle("Belgien")+
+  theme(plot.title = element_text(hjust = 0.5))+
+  ylab("Faelle pro Tag")+
+  geom_hline(yintercept=1000, linetype="dashed", color = "red")+
+  geom_hline(yintercept = 5000, linetype = "dashed", color = "red")+
+  scale_x_date(date_breaks = "1 month")
+
+#Bayern
+balken.bayern  <- ggplot(cases.per.day.bayern, aes(Datum, Bayern))+
+  geom_col(fill = "steelblue")+
+  ggtitle("Bayern")+
+  theme(plot.title = element_text(hjust = 0.5))+
+  ylab("Faelle pro Tag")+
+  geom_hline(yintercept=1000, linetype="dashed", color = "red")+
+  geom_hline(yintercept = 5000, linetype = "dashed", color = "red")+
+  scale_x_date(date_breaks = "1 month")                  
+
+#Tschechien
+balken.tschechien <- ggplot(cases.per.day.tschechien, aes(Datum, Tschechien))+
+  geom_col(fill = "steelblue")+
+  ggtitle("Tschechien")+
+  theme(plot.title = element_text(hjust = 0.5))+
+  ylab("Faelle pro Tag")+
+  geom_hline(yintercept=1000, linetype="dashed", color = "red")+
+  geom_hline(yintercept = 5000, linetype = "dashed", color = "red")+
+  scale_x_date(date_breaks = "1 month")
+
+#Schweden
+balken.schweden <- ggplot(cases.per.day.schweden, aes(Datum, Schweden))+
+  geom_col(fill = "steelblue")+
+  ggtitle("Schweden")+
+  theme(plot.title = element_text(hjust = 0.5))+
+  ylab("Faelle pro Tag")+
+  geom_hline(yintercept=1000, linetype="dashed", color = "red")+
+  geom_hline(yintercept = 5000, linetype = "dashed", color = "red")+
+  scale_x_date(date_breaks = "1 month")
 
