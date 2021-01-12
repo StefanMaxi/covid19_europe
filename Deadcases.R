53a880741faaf2f14994c3775d47d16bbc594dc3
sunmumu <58950430+szilvias@users.noreply.github.com>
2021-01-12 12:21:14 +0100

    Todesrate

--- Deadcases.R
+++ Deadcases.R
@@ -28,4 +28,32 @@ Tot <- merge(Tot, SchwedenTot1, by='Datum',all = TRUE)
 
 Tot[is.na(Tot)] <- 0
 view(Tot)
+#end date 27.12.2020
+deadCases2712 <- Tot[1:293,]
+deadCases2712
+BelDeath <- sum(deadCases2712$Belgien)
+TscDeath <- sum(deadCases2712$Tschechien)
+BayDeath <- sum(deadCases2712$Bayern)
+SweDeatj <- sum(deadCases2712$Schweden)
+sumofDeath <- c(BelDeath, TscDeath, BayDeath, SweDeatj)
 
+casesDaily2712 <- cases.per.day[1:362,]
+BelCase <- sum(casesDaily2712$Belgien)
+TscCase <- sum(casesDaily2712$Tschechien)
+BalCase <- sum(casesDaily2712$Bayern)
+SweCase <- sum(casesDaily2712$Schweden)
+sumofCases <- c(BelCase, TscCase, BalCase, SweCase)
+
+Todesrate <- label_percent()(sumofDeath/sumofCases)
+
+TodesrateDF <- data.frame(Land = c("Bayern", "Belgien", "Schweden", "Tschechien"), Todesrate = Todesrate)
+TodesrateDF$Todesrate <- as.numeric(sub("%", "", TodesrateDF$Todesrate))
+
+ggplot(TodesrateDF,aes(Land, Todesrate))+geom_bar(stat="identity",position = "dodge")+
+  ggtitle("Todesrate")+
+  theme(axis.text.x = element_text(angle = 45,hjust = 1,vjust = 1,colour = "black"),
+        plot.title = element_text(size="25",lineheight =.6,face = "bold",colour = "black"),
+        axis.title.x = element_text(colour = "black",size = "20"),
+        axis.title.y = element_text(colour = "black",size = "20"),
+        axis.text.y = element_text(angle=0, vjust=0.5, size=10,colour = "black"))+
+  theme(plot.title = element_text(hjust = 0.5))+labs(y="Todesrate(%)")
